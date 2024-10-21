90d75799c1755d187188a78f8539448b1d5c1d35
Nícolas de Souza Moreira <d2022014472@unifei.edu.br>
2024-10-20 21:25:36 -0300

    Player animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    Animator _playerAnimator;
     Rigidbody2D _playerRb;
     Vector2 _movement;
 
@@ -12,10 +13,11 @@ public class Player : MonoBehaviour
     void Awake()
     {
         _playerRb = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
 
-    public void OnMove(InputValue value)
+    void OnMove(InputValue value)
     {
         _movement = value.Get<Vector2>();
     }
@@ -23,8 +25,19 @@ public class Player : MonoBehaviour
     void MovePlayer()
     {
         _playerRb.velocity = _movement * movespeed;
+        
+        bool isRunning = Mathf.Abs(_playerRb.velocity.x) > Mathf.Epsilon;
+        _playerAnimator.SetBool("isRunning", isRunning);
+        if (isRunning)
+        {
+            Flip();
+        }
     }
 
+    void Flip()
+    {
+        transform.localScale = new Vector3(Mathf.Sign(_playerRb.velocity.x),1,1);
+    }
     void Update()
     {
         MovePlayer();
