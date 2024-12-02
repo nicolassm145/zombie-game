74344ac886aec7e822be9a0f29746373642c0f60
Nícolas de Souza Moreira <d2022014472@unifei.edu.br>
2024-12-02 18:28:05 -0300

    Add sounds, sfx

--- Assets/Scripts/UI/Button.cs
+++ Assets/Scripts/UI/Button.cs
@@ -6,20 +6,30 @@ public class Button : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     TextMeshProUGUI _buttonText;
     [SerializeField] Color normalColor = Color.white;
-    [SerializeField] Color hoverColor = Color.yellow; 
+    [SerializeField] Color hoverColor = Color.yellow;
+
+
+    [SerializeField] AudioClip buttonHover;
+
+
+    AudioSource _audioSource;
 
     void Awake()
-    { 
+    {
         _buttonText = GetComponentInChildren<TextMeshProUGUI>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         _buttonText.color = hoverColor;
+        _audioSource.PlayOneShot(buttonHover);
     }
 
     public void OnPointerExit(PointerEventData eventData)
-    { 
+    {
         _buttonText.color = normalColor;
     }
 }
+   
+  
--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
+
 public class Pistol : MonoBehaviour
 {
     // Bullet
